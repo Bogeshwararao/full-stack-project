3a2dc9e4c682155f14a08e3286f8e0786cbcd5c2
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-04-02 21:37:26 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
+require('dotenv').config()
 const express = require("express")
 const app= express()
 
+app.get('/', (req,res)=>{
+    res.json({msg:"welcome to app"})
+
+})
 
 
 
 
-
-app.listen(3001,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("listeneing on port 4000")
 })
+
